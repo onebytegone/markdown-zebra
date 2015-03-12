2115364354f8f474b44d32e311e0c61a2a0b3a8b
Ethan Smith <ethan@onebytegone.com>
2015-03-12 10:59:42 -0400

    Added support for table headers

--- markdown-zebra.js
+++ markdown-zebra.js
@@ -1,5 +1,5 @@
 /**
- * markdown-zebra v0.01
+ * markdown-zebra v0.1
  *
  * Copyright 2015 Ethan Smith
  * Licensed under the MIT license.
@@ -21,55 +21,115 @@
       'columnDelimiter': '|',
       'columnFill': '&nbsp;',
       'columnPadding': '&nbsp;',
+      'headerDividerFill': '-',
       'rowDelimiter': '\n',
       'newlineOutput': '<br>',
    };
 
+   // Create data type for table data
+   var TableStruct = function(rows, header) {
+      this.rows = rows;
+      this.header = header;
+   };
+
    zebra.parseMarkdownTable = function(tableStr) {
       var rows = tableStr.split(zebra.config.rowDelimiter);
-      var tableData = _.map(rows, function(row) {
+      var tableRows = _.map(rows, function(row) {
          var parsedRow = _.map(row.split(zebra.config.columnDelimiter), function(str) { return str.trim(); });
          return parsedRow.slice(1, parsedRow.length-1);
       });
+
+      var tableHeader = zebra.findHeaderInRows(tableRows);
+      if (tableHeader) {
+         tableRows.shift();  // remove the header row
+         tableRows.shift();  // remove the header divider
+      }
+
+      var tableData = new TableStruct(tableRows, tableHeader);
+
       return tableData;
    };
 
-   zebra.createMarkdownTable = function(tableData, preferredColWidths) {
+   zebra.findHeaderInRows = function(tableRows) {
+      var notEnoughRows = tableRows.length < 3;
+      if (notEnoughRows) {
+         return null;
+      }
+
+      // Make sure we have a header divider. Check that all the cols
+      // have the divider filling at least half of the field.
+      var hasHeaderDivider = _.reduce(tableRows[1], function(result, item) {
+         return result && item.split(zebra.config.headerDividerFill).length > item.length/2;
+      }, true);
+
+      if (hasHeaderDivider) {
+         return tableRows[0];
+      }
+
+      return null;
+   };
+
+   zebra.createMarkdownTable = function(tableRows, tableHeader, preferredColWidths) {
       if (preferredColWidths == null) {
          // TODO: this would be nice to have auto calculate based on width of the contents.
          preferredColWidths = 20;
       }
 
       if (!(preferredColWidths instanceof Array)) {
-         preferredColWidths = Array(_.first(tableData).length).fill(preferredColWidths);
+         preferredColWidths = Array(_.first(tableRows).length).fill(preferredColWidths);
       }
 
-      var tableStr = _.reduce(tableData, function(output, row) {
+      var tableStr = '';
+
+      if (tableHeader) {
+         tableStr += zebra.formatRow(tableHeader, preferredColWidths);
+         tableStr += zebra.generateHeaderDivider(preferredColWidths);
+      }
+
+      tableStr += _.reduce(tableRows, function(output, row) {
          var rowWidths = preferredColWidths.slice(0);
-         return output +
-            _.reduce(row, function(output, value) {
-               var width = rowWidths.shift() + 1;
-               return output +
-                  zebra.config.columnDelimiter +
-                  zebra.config.columnPadding +
-                  value +
-                  Array(Math.max(width - value.toString().length, 0)).join(zebra.config.columnFill) +
-                  zebra.config.columnPadding;
-            }, '') +
-            zebra.config.columnDelimiter +
-            zebra.config.rowDelimiter;
+         return output + zebra.formatRow(row, rowWidths);
       }, '');
 
       return tableStr;
    };
 
+   zebra.formatRow = function(items, widths) {
+      return _.reduce(items, function(output, value) {
+            var width = widths.shift() + 1;
+            return output + zebra.formatColumn(value, width);
+         }, '') +
+         zebra.config.columnDelimiter +
+         zebra.config.rowDelimiter;
+   };
+
+   zebra.generateHeaderDivider = function(widths) {
+      var cols = _.reduce(widths, function(output, width) {
+         return output + zebra.createColumnFill(zebra.config.headerDividerFill, width);
+      }, '');
+
+      return zebra.formatRow(cols, widths);
+   };
+
+   zebra.formatColumn = function(value, width) {
+      return zebra.config.columnDelimiter +
+         zebra.config.columnPadding +
+         value +
+         zebra.createColumnFill(zebra.config.columnFill, Math.max(width - value.toString().length, 0)) +
+         zebra.config.columnPadding;
+   };
+
+   zebra.createColumnFill = function(fill, width) {
+      return Array(width).join(fill);
+   };
+
    zebra.replaceNewlinesForOutput = function(str) {
       return str.replace('\n', zebra.config.newlineOutput);
-   }
+   };
 
    zebra.tidyUpTable = function(tableStr, preferredColWidths) {
       var data = zebra.parseMarkdownTable(tableStr);
-      var table = zebra.createMarkdownTable(data, preferredColWidths);
+      var table = zebra.createMarkdownTable(data.rows, data.header, preferredColWidths);
       return zebra.replaceNewlinesForOutput(table);
    };
 }.call(this));
